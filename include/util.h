03d386e8803b4ca3ee28fd332c64ee8505bc21e7
Josiah Worcester <josiahw@gmail.com>
2015-01-24 13:57:38 -0600

    Remove unused iconv.h dependency

--- include/util.h
+++ include/util.h
@@ -2,10 +2,7 @@
 #define UTIL_H
 
 #include <stddef.h>
-#include <iconv.h>
 
 void *grow_buf(void*, size_t*);
-size_t asiconv(iconv_t, const char*restrict, size_t, char*restrict*restrict,
-		size_t*restrict);
 
 #endif
